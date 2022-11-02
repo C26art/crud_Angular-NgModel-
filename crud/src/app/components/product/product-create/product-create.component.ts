3866bf989b58e452359e0e57d071ca98918fdd03
Cristian Mitidieri <cristianmc100@gmail.com>
2022-11-02 01:06:29 -0300

    feat: first-commit

---
+++ crud/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    nome: '',
+    quantidade: '',
+    fornecedor: '',
+    validade: '',
+    categoria: '',
+    id: ''
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  createProduct(): void {
+
+    this.productService.create(this.product).subscribe(() => {
+
+      this.productService.showMessage('Criado com sucesso!!!')
+      this.router.navigate(['/products'])
+    })
+
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
+
---
+++ crud/src/app/components/product/product.model.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: string
+  nome: string
+  quantidade: string
+  fornecedor: string
+  validade: string
+  categoria: string
+}
